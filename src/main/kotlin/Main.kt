87eb9bf8804f96c2b18dbf74b528e707694954d2
Rua Fuller <ruafuller@gmail.com>
2026-02-12 21:02:45 +0000

    Implement basic object structuring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,22 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+data class ComplexNumber(val real: Int, val imaginary: Int)
+data class Location(val boardIndex: ComplexNumber, val timeplane: Int = 0)
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+// TODO: move fields to constructor
+class Game(var timeplanes: MutableList<Timeplane>, var limbo: MutableSet<Board>)
+class Timeplane(var boards: MutableMap<ComplexNumber, Board>)
+class Board(var location: Location?, val parent: Location? = null) {
+    override fun toString(): String {
+        return "Board {\n    location: $location\n}" // JSON notation
     }
+}
+
+fun main() {
+    val board = Board(Location(ComplexNumber(0, 0)))
+    testBoard(board)
+}
+
+fun testBoard(board: Board) {
+    println("This is, in fact, a Board!\n\n$board")
 }
