bba60f31e0094d3d54f03ccbee01c02376c56d8a
LudoBC <ludobvw@gmail.com>
2026-02-14 17:16:25 +0100

    Type restricive order actions

--- src/main/kotlin/nodomain/seven/dip/orders/Orders.kt
+++ src/main/kotlin/nodomain/seven/dip/orders/Orders.kt
@@ -6,13 +6,13 @@ import nodomain.seven.dip.provinces.Province
 import kotlin.enums.enumEntries
 
 sealed interface Piece {
-    val board: Location
+    val at: Space
 
     val holds: HoldOrder
         get() = HoldOrder(this)
 
     infix fun M(to: Space): MoveOrder = MoveOrder(this, Moves(to))
-    infix fun M(to: Province): MoveOrder = M(Space(to, board))
+    infix fun M(to: Province): MoveOrder = M(Space(to, at.board))
 
     infix fun S(supporting: () -> Order): SupportOrder {
         val order = supporting();
@@ -27,12 +27,12 @@ fun T(boardIndex: ComplexNumber, timeplane: Int): Location  = Location(boardInde
 operator fun Location.get(province: Province): Space = Space(province, this)
 
 @JvmInline
-value class Army(val at: Space): Piece {
-    override val board get() = at.board
-}
+value class Army(override val at: Space): Piece
 infix fun Location.A(province: Province): Army = Army(Space(province, this))
 
-abstract class Order(val piece: Piece, val action: Action, val symbol: String) {
+abstract class Order(val piece: Piece, val symbol: String) {
+    abstract val action: Action
+
     override fun toString(): String =  "$piece$symbol$action"
 }
 
@@ -46,17 +46,19 @@ enum class TimeFlare(val direction: ComplexNumber) {
 sealed interface Action {}
 
 data object Holds: Action
-class HoldOrder(piece: Piece): Order(piece, Holds, " ")
+class HoldOrder(piece: Piece): Order(piece, " ") {
+    override val action: Holds = Holds
+}
 
 @JvmInline
 value class Moves(val to: Space): Action
-class MoveOrder(piece: Piece, action: Moves, var flare: TimeFlare? = null): Order(piece, action, " - ") {
+class MoveOrder(piece: Piece, override val action: Moves, var flare: TimeFlare? = null): Order(piece, " - ") {
     infix fun i(timeFlare: Int): Order {
-        if (action is Moves) flare = enumEntries<TimeFlare>()[timeFlare % 4];
+        flare = enumEntries<TimeFlare>()[timeFlare % 4];
         return this;
     }
 }
 
 @JvmInline
 value class Supports(val order: Order): Action
-class SupportOrder(piece: Piece, action: Supports): Order(piece, action, " S ")
+class SupportOrder(piece: Piece, override val action: Supports): Order(piece, " S ")
