21ccf84fa2cd76961f2b149bbd97668b4884109a
LudoBC <ludobvw@gmail.com>
2026-02-16 20:55:28 +0100

    reworked order sorting

--- src/main/kotlin/nodomain/seven/dip/adjudication/Adjudicator.kt
+++ src/main/kotlin/nodomain/seven/dip/adjudication/Adjudicator.kt
@@ -1,21 +1,17 @@
 package nodomain.seven.dip.adjudication
 
 import nodomain.seven.dip.orders.MoveOrder
-import nodomain.seven.dip.orders.Moves
 import nodomain.seven.dip.orders.Order
 import nodomain.seven.dip.orders.SupportOrder
 import nodomain.seven.dip.orders.TemporalFlare
 
-fun sortOrders(orders: List<Order>): Pair<Map<TemporalFlare, List<MoveOrder>>, Map<SupportOrder, Int>> {
-    val moves: MutableSet<MoveOrder> = mutableSetOf()
-    val supports: MutableSet<SupportOrder> = mutableSetOf()
+fun sortOrders(orders: List<Order>): Pair<Map<TemporalFlare, List<MoveOrder>>, List<SupportOrder>> {
+    val moves: MutableList<MoveOrder> = mutableListOf()
+    val supports: MutableList<SupportOrder> = mutableListOf()
     for (order in orders) when (order) {
         is MoveOrder -> moves += order
         is SupportOrder -> supports += order
     }
-    return Pair(moves.groupBy { it.flare !! }, supports.associateWith { 0 })
+    return Pair(moves.groupBy { it.flare !! }, supports)
 }
-
-fun TemporalFlare.thisMethodNeedsABetterName(flag: Int): Int = flag or (1 shr ordinal)
-
 
