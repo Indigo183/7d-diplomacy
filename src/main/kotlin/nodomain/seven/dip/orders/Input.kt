2a438de06b282958fd85c480640f4d57a7ff97e2
LudoBC <ludobvw@gmail.com>
2026-02-17 11:53:27 +0100

    comments at the top into javaDoc

--- src/main/kotlin/nodomain/seven/dip/orders/Input.kt
+++ src/main/kotlin/nodomain/seven/dip/orders/Input.kt
@@ -4,22 +4,18 @@ import nodomain.seven.dip.adjudication.isAdjacentTo
 import nodomain.seven.dip.game.Game
 import nodomain.seven.dip.provinces.Player
 
-// Checks that:
-// 1. the ordered unit exists
-// 2. if a player is passed, the player owns the unit
-// 3. if the order is a move, the destination exists
-// 4. the origin and destination are adjacent
-// and ignores the possibility of convoys
+/** Checks that:
+ * 1. the ordered unit exists
+ * 2. if a player is passed, the player owns the unit
+ * 3. if the order is a move, the destination exists
+ * 4. the origin and destination are adjacent and ignores the possibility of convoys*/
 fun Game.isValid(order: Order, player: Player? = null): Boolean {
-    // 1
-    val board = getBoard(order.piece.location.boardIndex) ?: return false
-    // 1 & 2
+    val board = getBoard(order.piece.location.boardIndex) ?: return false // 1 (partly)
     val unitExistsAndOwned = when(player) {
         null -> board.pieces.any { (_, countryPieces) -> order.piece.location.province in countryPieces }
         else -> board.pieces[player]?.contains(order.piece.location.province) ?: false
     }
-    if (!unitExistsAndOwned) return false
-    // 4
+    if (!unitExistsAndOwned) return false // 1 & 2
     val destination = when(order) {
         is SupportOrder if order.action.order is MoveOrder -> order.action.order.action.to
         is SupportOrder -> order.action.order.piece.location
@@ -27,9 +23,9 @@ fun Game.isValid(order: Order, player: Player? = null): Boolean {
             getBoard(order.action.to.boardIndex)?: return false // 3
             order.action.to
         }
-        is HoldOrder -> return true //hold orders do not have a destination
+        is HoldOrder -> return true //hold orders do not have a destination, thus 4 is trivial
     }
-    return order.piece.location isAdjacentTo destination
+    return order.piece.location isAdjacentTo destination // 4
 }
 
 fun input(player: Player, orders: List<Order>) = println()
