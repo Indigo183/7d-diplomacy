8462ef04fc3ec6efee795205b01ad89d952a9a01
LudoBC <ludobvw@gmail.com>
2026-02-20 21:12:54 +0100

    first version move strength analyses UNFINISHED

--- src/main/kotlin/nodomain/seven/dip/adjudication/Adjudicator.kt
+++ src/main/kotlin/nodomain/seven/dip/adjudication/Adjudicator.kt
@@ -5,6 +5,8 @@ import nodomain.seven.dip.orders.MoveOrder
 import nodomain.seven.dip.orders.Order
 import nodomain.seven.dip.orders.SupportOrder
 import nodomain.seven.dip.orders.TemporalFlare
+import nodomain.seven.dip.provinces.Player
+import nodomain.seven.dip.utils.Location
 
 fun Game.sortOrders(orders: List<Order>): Pair<Map<TemporalFlare, List<MoveOrder>>, List<SupportOrder>> {
     // For future optimisation
@@ -16,4 +18,31 @@ fun Game.sortOrders(orders: List<Order>): Pair<Map<TemporalFlare, List<MoveOrder
         else -> {}
     }
     return Pair(moves.groupBy { it.flare !! }, supports)
+}
+
+//TODO
+fun moveStrength(moves: List<MoveOrder>, supports: List<SupportOrder>, pieces: Map<Location, Player>) {
+    class MoveAnalyse(val order: MoveOrder, var strength: Int = 1)
+    val analyse = moves.associateWith { MoveAnalyse(it) }
+    val nonCutSupports = supports.asSequence().filterNot(::isCut)
+    nonCutSupports
+        .filter { it.action.order is MoveOrder}
+        .forEach { analyse[it.action.order]?.strength++ }
+    analyse.values
+        .groupBy { it.order.action.to }
+        .mapValues { (destination, orders) ->
+            val topStrength = orders.maxOf { it.strength }
+            val presumptiveMove = if (orders.count { it.strength == topStrength } == 1)
+                orders.maxBy { it.strength } else null
+            when (pieces[destination]) {
+                null -> presumptiveMove // ?: Bounce()
+                pieces[presumptiveMove?.order?.piece?.location] -> {} // dependence on destination (or bounce if holds)
+                else -> {} // dependence on destination, or bounce if holds at topStrength or better
+            }
+        }
+}
+
+//TODO
+fun isCut(support: SupportOrder): Boolean {
+    return false
 }
