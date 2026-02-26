dd8608c1366d4efb6855735c5dd46df424820864
Rua Fuller <ruafuller@gmail.com>
2026-02-26 08:37:13 +0000

    Begin implementation of full game test in main

--- src/main/kotlin/nodomain/seven/dip/Main.kt
+++ src/main/kotlin/nodomain/seven/dip/Main.kt
@@ -2,27 +2,21 @@ package nodomain.seven.dip
 
 import nodomain.seven.dip.game.Game
 import nodomain.seven.dip.game.Board
-import nodomain.seven.dip.provinces.RomanPlayers.Cato
-import nodomain.seven.dip.provinces.RomanPlayers.Pompey
-import nodomain.seven.dip.provinces.Romans.CAT
-import nodomain.seven.dip.provinces.Romans.POM
+import nodomain.seven.dip.adjudication.*
+import nodomain.seven.dip.orders.*
+import nodomain.seven.dip.provinces.RomanPlayers.*
+import nodomain.seven.dip.provinces.Romans.*
 import nodomain.seven.dip.utils.*
-import nodomain.seven.dip.utils.BoardIndex
 
 fun main() {
+    val origin = T(0.c , 0)
     val game = Game()
-    game.addChild(
-        game.getBoard(BoardIndex(0.c))!!,
-        BoardIndex(1.c),
-        mapOf(CAT to Cato, POM to Pompey),
-        mapOf(CAT to Cato, POM to Pompey),
-    )
-
-    for (timeplane in game.timeplanes) {
-        for (board in timeplane.values) {
-            testBoard(board)
-        }
-    }
+    
+    game.input(listOf(
+        origin A CAT M CAE i 3,
+        origin A POM M BRU i 3
+    ))
+    game.adjudicate()
 }
 
 fun testBoard(board: Board) {
