b002756d54941a0a71f74696fa7c0008448ee4aa
LudoBC <ludobvw@gmail.com>
2026-03-01 21:25:55 +0100

    Unit test for isValid

---
+++ src/test/kotlin/nodomain/seven/dip/orders/InputTest.kt
@@ -0,0 +1,21 @@
+package nodomain.seven.dip.orders
+
+import nodomain.seven.dip.game.Game
+import nodomain.seven.dip.provinces.A
+import nodomain.seven.dip.provinces.StandardPlayer
+import nodomain.seven.dip.provinces.StandardProvince.*
+import nodomain.seven.dip.provinces.setup
+import org.assertj.core.api.WithAssertions
+import kotlin.test.Test
+
+class InputTest: WithAssertions {
+    @Test
+    fun isValidTest() {
+        //A Naples Supports A Apulia - Venice
+        val testOrder = A[NAP] S { A[APU] M VEN}
+
+        val result = Game(setup<StandardPlayer>()).isValid(testOrder)
+
+        assertThat(result).isFalse
+    }
+}
