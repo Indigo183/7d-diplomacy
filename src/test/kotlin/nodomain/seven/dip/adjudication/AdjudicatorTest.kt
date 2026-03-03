44299b4c305f2edaed17de4c9340d408b7d0ed44
LudoBC <ludobvw@gmail.com>
2026-03-03 23:25:13 +0100

    Circulair movement functions on adjudicator level

--- src/test/kotlin/nodomain/seven/dip/adjudication/AdjudicatorTest.kt
+++ src/test/kotlin/nodomain/seven/dip/adjudication/AdjudicatorTest.kt
@@ -19,7 +19,6 @@ class AdjudicatorTest: WithAssertions {
 
         val results = Adjudicator(moves, listOf(), pieces).moveResults
 
-        // println(results)
         assertThat(results).contains(Bounce(origin[BRU]))
     }
 
@@ -34,7 +33,6 @@ class AdjudicatorTest: WithAssertions {
 
         val results = Adjudicator(moves, listOf(), pieces).moveResults
 
-        // println(results)
         assertThat(results).containsAll(moves.map { SuccessfulMove(it) })
     }
 
@@ -49,5 +47,34 @@ class AdjudicatorTest: WithAssertions {
         val results = Adjudicator(moves, listOf(), pieces).moveResults
 
         assertThat(results).doesNotContainAnyElementsOf(moves.map { SuccessfulMove(it) })
+    }
+
+    @Test
+    fun whenTwoUnitsEntersEachOthersLocation_thenTheirMovesAreNotSuccessful() {
+        val origin = T(0.c , 0)
+        val pieces = mapOf(origin[CAT] to Cato, origin[BRU] to Pompey)
+        val moves = listOf(
+            origin A CAT M BRU i 3,
+            origin A BRU M CAT i 3
+        )
+
+        val results = Adjudicator(moves, listOf(), pieces).moveResults
+
+        assertThat(results).doesNotContainAnyElementsOf(moves.map { SuccessfulMove(it) })
+    }
+
+    @Test
+    fun whenThreeUnitsEntersEachOthersLocationInACircle_thenTheirMovesAreSuccessful() {
+        val origin = T(0.c , 0)
+        val pieces = mapOf(origin[CAT] to Cato, origin[BRU] to Pompey, origin[CAE] to Pompey)
+        val moves = listOf(
+            origin A CAT M BRU i 3,
+            origin A BRU M CAE i 3,
+            origin A CAE M CAT i 3
+        )
+
+        val results = Adjudicator(moves, listOf(), pieces).moveResults
+
+        assertThat(results).containsAll(moves.map { SuccessfulMove(it) })
     }
 }
