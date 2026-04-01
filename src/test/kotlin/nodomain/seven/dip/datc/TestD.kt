c786b6bba7b039cae0c7a61e4e83f33e6e3b3e5a
LudoBC <ludobvw@gmail.com>
2026-04-01 22:14:23 +0200

    removed changed to test D that ware made due to lack of fleets

--- src/test/kotlin/nodomain/seven/dip/datc/TestD.kt
+++ src/test/kotlin/nodomain/seven/dip/datc/TestD.kt
@@ -11,10 +11,10 @@ import kotlin.test.Test
  */
 object TestD: WithAssertionsDATC {
     @Test
-    fun `6_D_1 TEST CASE, SUPPORTED HOLD CAN PREVENT DISLODGEMENT`() { // Modified due to involving sea regions
+    fun `6_D_1 TEST CASE, SUPPORTED HOLD CAN PREVENT DISLODGEMENT`() {
         """
         |Austria:
-        |A Apulia Supports A Trieste - Venice
+        |F Adriatic Sea Supports A Trieste - Venice
         |A Trieste - Venice
         |
         |Italy:
@@ -27,7 +27,7 @@ object TestD: WithAssertionsDATC {
     fun `6_D_2 TEST CASE, A MOVE CUTS SUPPORT ON HOLD`() {
         val result = """
         |Austria:
-        |A Apulia Supports A Trieste - Venice
+        |F Adriatic Sea Supports A Trieste - Venice
         |A Trieste - Venice
         |A Vienna - Tyrolia
         |
@@ -42,65 +42,65 @@ object TestD: WithAssertionsDATC {
     }
 
     @Test
-    fun `6_D_3 TEST CASE, A MOVE CUTS SUPPORT ON MOVE`() { // Modified due to involving sea regions
+    fun `6_D_3 TEST CASE, A MOVE CUTS SUPPORT ON MOVE`() {
         """
         |Austria:
-        |A Tuscany Supports A Trieste - Venice
+        |F Adriatic Sea Supports A Trieste - Venice
         |A Trieste - Venice
         |
         |Italy:
         |A Venice Hold
-        |A Rome - Tuscany
+        |F Ionian Sea - Adriatic Sea
         |""".parse().adjudicateAsDOTC().andAssertThatNothingMoved()
     }
 
     @Test
-    fun `6_D_4 TEST CASE, SUPPORT TO HOLD ON UNIT SUPPORTING A HOLD ALLOWED`() { // Modified due to involving sea regions
+    fun `6_D_4 TEST CASE, SUPPORT TO HOLD ON UNIT SUPPORTING A HOLD ALLOWED`() {
         """
         |Germany:
-        |A Berlin Supports A Kiel
+        |F Berlin Supports A Kiel
         |A Kiel Supports A Berlin
         |
         |Russia:
-        |A Silesia Supports A Prussia - Berlin
-        |A Munich - Berlin
+        |F Baltic Sea Supports A Prussia - Berlin
+        |A Prussia - Berlin
         |""".parse().adjudicateAsDOTC().andAssertThatNothingMoved()
     }
 
     @Test
-    fun `6_D_5 TEST CASE, SUPPORT TO HOLD ON UNIT SUPPORTING A MOVE ALLOWED`() { // Modified due to involving sea regions
+    fun `6_D_5 TEST CASE, SUPPORT TO HOLD ON UNIT SUPPORTING A MOVE ALLOWED`() {
         val result = """
         |Germany:
-        |A Munich Supports A Berlin - Silesia
-        |A Kiel Supports A Munich
-        |A Berlin - Silesia
+        |A Berlin Supports A Munich - Silesia
+        |F Kiel Supports A Berlin
+        |A Munich - Silesia
         |
-        |Austria:
-        |A Bohemia Supports A Tyrolia - Munich
-        |A Tyrolia - Munich
+        |Russia: 
+        |F Baltic Sea Supports A Prussia - Berlin
+        |A Prussia - Berlin
         |""".parse().adjudicateAsDOTC()
 
-        assertThat(result.pieces).containsEntry(MUN, Germany)
+        assertThat(result.pieces).containsEntry(BER, Germany)
     }
 
     //6.D.6. TEST CASE, SUPPORT TO HOLD ON CONVOYING UNIT ALLOWED
 
     @Test
-    fun `6_D_7 TEST CASE, SUPPORT TO HOLD ON MOVING UNIT NOT ALLOWED`() { // Modified due to involving sea regions
+    fun `6_D_7 TEST CASE, SUPPORT TO HOLD ON MOVING UNIT NOT ALLOWED`() {
         val result = """
         |Germany:
-        |A Warsaw - Ukraine
-        |A Prussia Supports A Warsaw
+        |F Baltic Sea - Sweden
+        |F Prussia Supports F Baltic Sea
         |
         |Russia:
-        |A Livonia - Warsaw
-        |A Moscow Supports A Livonia - Warsaw
-        |A Sevastopol - Ukraine
+        |F Livonia - Baltic Sea
+        |F Gulf of Bothnia Supports F Livonia - Baltic Sea
+        |A Finland - Sweden
         |""".parse().adjudicateAsDOTC()
 
         assertThat(result.pieces)
-            .containsEntry(WAR, Russia)
-            .doesNotContainEntry(UKR, Germany)
+            .containsEntry(BAL, Russia)
+            .doesNotContainEntry(SWE, Germany)
     }
 
     //6_D_8 TEST CASE, FAILED CONVOY CANNOT RECEIVE HOLD SUPPORT
@@ -127,7 +127,7 @@ object TestD: WithAssertionsDATC {
         """
         |Germany:
         |A Berlin Hold
-        |A Kiel - Berlin
+        |F Kiel - Berlin
         |A Munich Supports A Kiel - Berlin
         |""".parse().adjudicateAsDOTC().andAssertThatNothingMoved()
     }
@@ -137,7 +137,7 @@ object TestD: WithAssertionsDATC {
         """
         |Germany:
         |A Berlin - Prussia
-        |A Kiel - Berlin
+        |F Kiel - Berlin
         |A Munich Supports A Kiel - Berlin
         |
         |Russia:
@@ -149,7 +149,7 @@ object TestD: WithAssertionsDATC {
     fun `6_D_12 TEST CASE, SUPPORTING A FOREIGN UNIT TO DISLODGE OWN UNIT PROHIBITED`() {
         """
         |Austria:
-        |A Trieste Hold
+        |F Trieste Hold
         |A Vienna Supports A Venice - Trieste
         |
         |Italy:
@@ -158,29 +158,29 @@ object TestD: WithAssertionsDATC {
     }
 
     @Test
-    fun `6_D_13 TEST CASE, SUPPORTING A FOREIGN UNIT TO DISLODGE A RETURNING OWN UNIT PROHIBITED`() { // Modified due to involving sea regions
+    fun `6_D_13 TEST CASE, SUPPORTING A FOREIGN UNIT TO DISLODGE A RETURNING OWN UNIT PROHIBITED`() {
         """
         |Austria:
-        |A Trieste - Albania
+        |F Trieste - Adriatic Sea
         |A Vienna Supports A Venice - Trieste
         |
         |Italy:
         |A Venice - Trieste
-        |A Greece - Albania
+        |F Apulia - Adriatic Sea
         |""".parse().adjudicateAsDOTC().andAssertThatNothingMoved()
     }
 
     @Test
-    fun `6_D_14 TEST CASE, SUPPORTING A FOREIGN UNIT IS NOT ENOUGH TO PREVENT DISLODGEMENT`() { // Modified due to involving sea regions
+    fun `6_D_14 TEST CASE, SUPPORTING A FOREIGN UNIT IS NOT ENOUGH TO PREVENT DISLODGEMENT`() {
         val result = """
         |Austria:
-        |A Trieste Hold
+        |F Trieste Hold
         |A Vienna Supports A Venice - Trieste
         |
         |Italy:
         |A Venice - Trieste
         |A Tyrolia Supports A Venice - Trieste
-        |A Albania Supports A Venice - Trieste
+        |F Adriatic Sea Supports A Venice - Trieste
         |""".parse().adjudicateAsDOTC()
 
         assertThat(result.pieces)
@@ -188,14 +188,14 @@ object TestD: WithAssertionsDATC {
     }
 
     @Test
-    fun `6_D_15 TEST CASE, DEFENDER CANNOT CUT SUPPORT FOR ATTACK ON ITSELF`() { // Modified due to involving sea regions
+    fun `6_D_15 TEST CASE, DEFENDER CANNOT CUT SUPPORT FOR ATTACK ON ITSELF`() {
         val result = """
         |Russia:
-        |A Constantinople Supports A Armenia - Ankara
-        |A Armenia - Ankara
+        |F Constantinople Supports F Black Sea - Ankara
+        |F Black Sea - Ankara
         |
         |Turkey:
-        |A Ankara - Constantinople
+        |F Ankara - Constantinople
         |""".parse().adjudicateAsDOTC()
 
         assertThat(result.pieces).containsEntry(ANK, Russia)
@@ -204,54 +204,53 @@ object TestD: WithAssertionsDATC {
     //6.D.16. TEST CASE, CONVOYING A UNIT DISLODGING A UNIT OF SAME POWER IS ALLOWED
 
     @Test
-    fun `6_D_17 TEST CASE, DISLODGEMENT CUTS SUPPORTS`() { // Modified due to involving sea regions
+    fun `6_D_17 TEST CASE, DISLODGEMENT CUTS SUPPORTS`() {
         val result = """
         |Russia:
-        |A Ankara Supports A Sevastopol - Armenia
-        |A Sevastopol - Armenia
+        |F Constantinople Supports F Black Sea - Ankara
+        |F Black Sea - Ankara
         |
         |Turkey:
+        |F Ankara - Constantinople
+        |A Smyrna Supports F Ankara - Constantinople
         |A Armenia - Ankara
-        |A Smyrna Supports A Armenia - Ankara
-        |A Syria - Armenia
         |""".parse().adjudicateAsDOTC()
 
         assertThat(result.pieces)
-            .containsEntry(ANK, Turkey)
-            .doesNotContainEntry(ARM, Russia)
+            .containsEntry(CON, Turkey)
+            .doesNotContainEntry(ANK, Russia)
     }
 
     @Test
-    fun `6_D_18 TEST CASE, A SURVIVING UNIT WILL SUSTAIN SUPPORT`() { // Modified due to involving sea regions
+    fun `6_D_18 TEST CASE, A SURVIVING UNIT WILL SUSTAIN SUPPORT`() {
         val result = """
         |Russia:
-        |A Ankara Supports A Sevastopol - Armenia
-        |A Sevastopol - Armenia
-        |A Constantinople Supports A Ankara
+        |F Constantinople Supports F Black Sea - Ankara
+        |F Black Sea - Ankara
+        |A Bulgaria Supports F Constantinople
         |
         |Turkey:
+        |F Ankara - Constantinople
+        |A Smyrna Supports F Ankara - Constantinople
         |A Armenia - Ankara
-        |A Smyrna Supports A Armenia - Ankara
-        |A Syria - Armenia
-        |""".parse().adjudicateAsDOTC()
-
-        assertThat(result.pieces).containsEntry(ARM, Russia)
-    }
-
-    @Test
-    fun `6_D_19 TEST CASE, EVEN WHEN SURVIVING IS IN ALTERNATIVE WAY`() { // Modified due to involving sea regions
-        val result = """
-        |Russia:
-        |A Constantinople Supports A Armenia - Ankara
-        |A Armenia - Ankara
-        |A Smyrna Supports A Ankara - Constantinople
-        |
-        |Turkey:
-        |A Ankara - Constantinople
-        |The Russian fleet in Constantinople is not dislodged, because one of the supports is of Russian origin. The support from Black Sea to Ankara will sustain and the fleet in Ankara will be dislodged.
         |""".parse().adjudicateAsDOTC()
 
         assertThat(result.pieces).containsEntry(ANK, Russia)
+    }
+
+    @Test
+    fun `6_D_19 TEST CASE, EVEN WHEN SURVIVING IS IN ALTERNATIVE WAY`() {
+        val result = """
+        |Russia:
+        |F Constantinople Supports F Black Sea - Ankara
+        |F Black Sea - Ankara
+        |A Smyrna Supports F Ankara - Constantinople
+        |
+        |Turkey:
+        |F Ankara - Constantinople
+        |""".parse().adjudicateAsDOTC()
+
+        assertThat(result.pieces).containsEntry(CON, Russia)
     }
 
     //6.D.20. TEST CASE, UNIT CANNOT CUT SUPPORT OF ITS OWN COUNTRY
@@ -260,7 +259,7 @@ object TestD: WithAssertionsDATC {
     fun `6_D_21 TEST CASE, DISLODGING DOES NOT CANCEL A SUPPORT CUT`() {
         val result = """
         |Austria:
-        |A Trieste Hold
+        |F Trieste Hold
         |
         |Italy:
         |A Venice - Trieste
@@ -284,14 +283,18 @@ object TestD: WithAssertionsDATC {
     //6.D.23. TEST CASE, IMPOSSIBLE COAST MOVE CANNOT BE SUPPORTED
 
     @Test
-    fun `6_D_24 TEST CASE, IMPOSSIBLE ARMY MOVE CANNOT BE SUPPORTED`() { // Modified due to involving sea regions
+    fun `6_D_24 TEST CASE, IMPOSSIBLE ARMY MOVE CANNOT BE SUPPORTED`() {
         """
         |France:
-        |A Marseilles - Portugal
-        |A Spain Supports A Marseilles - Portugal
+        |A Marseilles - Gulf of Lyon
+        |F Spain(sc) Supports A Marseilles - Gulf of Lyon
         |
         |Italy:
-        |A Portugal Hold
+        |F Gulf of Lyon Hold
+        |
+        |Turkey:
+        |F Tyrrhenian Sea Supports F Western Mediterranean - Gulf of Lyon
+        |F Western Mediterranean - Gulf of Lyon
         |""".parse().adjudicateAsDOTC().andAssertThatNothingMoved()
     }
 
@@ -300,10 +303,10 @@ object TestD: WithAssertionsDATC {
         """
         |Germany:
         |A Berlin Supports A Prussia
-        |A Kiel Supports A Berlin
+        |F Kiel Supports A Berlin
         |
         |Russia:
-        |A Munich Supports A Prussia - Berlin
+        |F Baltic Sea Supports A Prussia - Berlin
         |A Prussia - Berlin
         |""".parse().adjudicateAsDOTC().andAssertThatNothingMoved()
     }
@@ -313,10 +316,10 @@ object TestD: WithAssertionsDATC {
         """
         |Germany:
         |A Berlin Supports A Prussia - Silesia
-        |A Kiel Supports A Berlin
+        |F Kiel Supports A Berlin
         |
         |Russia:
-        |A Munich Supports A Prussia - Berlin
+        |F Baltic Sea Supports A Prussia - Berlin
         |A Prussia - Berlin
         |""".parse().adjudicateAsDOTC().andAssertThatNothingMoved()
     }
@@ -327,14 +330,14 @@ object TestD: WithAssertionsDATC {
     fun `6_D_28 TEST CASE, IMPOSSIBLE MOVE AND SUPPORT`() {
         """
         |Austria:
-        |A Budapest Supports A Rumania
+        |A Budapest Supports F Rumania
         |
         |Russia:
-        |A Rumania - Holland
+        |F Rumania - Holland
         |
         |Turkey:
-        |A Serbia - Rumania
-        |A Bulgaria Supports A Serbia - Rumania
+        |F Black Sea - Rumania
+        |A Bulgaria Supports F Black Sea - Rumania
         |""".parse().adjudicateAsDOTC().andAssertThatNothingMoved()
     }
 
@@ -366,21 +369,21 @@ object TestD: WithAssertionsDATC {
     }
 
     @Test
-    fun `6_D_34 TEST CASE, SUPPORT TARGETING OWN AREA NOT ALLOWED`() { // Modified due to involving sea regions
+    fun `6_D_34 TEST CASE, SUPPORT TARGETING OWN AREA NOT ALLOWED`() {
         val result = """
         |Germany:
-        |A Berlin - Silesia
-        |A Munich Supports A Berlin - Silesia
-        |A Prussia Supports A Berlin - Silesia
+        |A Berlin - Prussia
+        |A Silesia Supports A Berlin - Prussia
+        |F Baltic Sea Supports A Berlin - Prussia
         |
         |Italy:
-        |A Silesia Supports Warsaw - Silesia
+        |A Prussia Supports Livonia - Prussia
         |
         |Russia:
-        |A Galicia Supports A Warsaw - Silesia
-        |A Warsaw - Silesia
+        |A Warsaw Supports A Livonia - Prussia
+        |A Livonia - Prussia
         |""".parse().adjudicateAsDOTC()
 
-        assertThat(result.pieces).containsEntry(SIL, Germany)
+        assertThat(result.pieces).containsEntry(PRU, Germany)
     }
 }
