e102f9dd97e23bb3d153e765fb46c6f38ca44b84
Rua Fuller <ruafuller@gmail.com>
2026-04-05 21:27:56 +0100

    Pass DATC-H 1 through 3

--- src/test/kotlin/nodomain/seven/dip/datc/TestH.kt
+++ src/test/kotlin/nodomain/seven/dip/datc/TestH.kt
@@ -1,17 +1,18 @@
 package nodomain.seven.dip.datc
 
 import nodomain.seven.dip.game.GameState.*
-import nodomain.seven.dip.orders.RetreatOrder
+import nodomain.seven.dip.orders.HoldOrder
 import nodomain.seven.dip.provinces.StandardCoast.*
 import nodomain.seven.dip.provinces.StandardInLand.*
 import nodomain.seven.dip.provinces.StandardPlayer.*
+import nodomain.seven.dip.provinces.StandardSea.*
 import kotlin.test.Test
 
 //6.H. TEST CASES, RETREATING
 object TestH: WithAssertionsDATC {
     @Test
     fun `6_H_1 TEST CASE, NO SUPPORTS DURING RETREAT`() { // Modified due to involving sea regions
-        val testGame = """
+        val game = """
         |Austria:
         |F Trieste Hold
         |A Serbia Hold
@@ -26,7 +27,7 @@ object TestH: WithAssertionsDATC {
         |F Aegean Sea Supports F Ionian Sea - Greece
         |""".parse().adjudicateAsDATC()
 
-        assertThat(testGame.requiredRetreats.locations).containsExactlyInAnyOrder(*retreatsIn(GRE, TRI))
+        assertThat(game.requiredRetreats.locations).containsExactlyInAnyOrder(*retreatsIn(GRE, TRI))
 
         """
         |Austria:
@@ -35,16 +36,15 @@ object TestH: WithAssertionsDATC {
         |
         |Turkey:
         |F Greece - Albania
-        |""".parse(RETREATS).adjudicateAsDATC(game = testGame)
+        |""".parse(RETREATS).adjudicateAsDATC(game = game)
 
-        assertThat(testGame.pieces)
-            .doesNotContainEntry(ALB, Austria)
-            .doesNotContainEntry(ALB, Turkey)
+        assertThat(game.pieces)
+            .doesNotContainKey(ALB)
     }
 
     @Test
     fun `6_H_2 TEST CASE, NO SUPPORTS FROM RETREATING UNIT`() {
-        """
+        val game = """
         |England:
         |A Liverpool - Edinburgh
         |F Yorkshire Supports A Liverpool - Edinburgh
@@ -59,8 +59,10 @@ object TestH: WithAssertionsDATC {
         |A Sweden Supports A Finland - Norway
         |A Finland - Norway
         |F Holland Hold
-        |The English fleet in Norway and the Russian fleets in Edinburgh and Holland are dislodged. If the following retreat orders are given:
         |""".parse().adjudicateAsDATC()
+
+        assertThat(game.requiredRetreats.locations).containsExactlyInAnyOrder(*retreatsIn(NWY, EDI, HOL))
+
         """
         |England:
         |F Norway - North Sea
@@ -68,15 +70,18 @@ object TestH: WithAssertionsDATC {
         |Russia:
         |F Edinburgh - North Sea
         |F Holland Supports F Edinburgh - North Sea
-        |Although the fleet in Holland may receive an order, it may not support (it is disbanded). The English fleet in Norway and the Russian fleet in Edinburgh bounce and are disbanded.
-        |"""
+        |""".parse(RETREATS).adjudicateAsDATC(game = game)
+
+        assertThat(game.pieces)
+            .doesNotContainKey(NTH)
+            .doesNotContainEntry(HOL, Russia)
     }
 
     //6.H.3. TEST CASE, NO CONVOY DURING RETREAT
 
     @Test
     fun `6_H_4 TEST CASE, NO OTHER MOVES DURING RETREAT`() {
-        """
+        val game = """
         |England:
         |F North Sea Hold
         |A Holland Hold
@@ -84,32 +89,39 @@ object TestH: WithAssertionsDATC {
         |Germany:
         |F Kiel Supports A Ruhr - Holland
         |A Ruhr - Holland
-        |The English army in Holland is dislodged. If England orders the following in retreat:
         |""".parse().adjudicateAsDATC()
+
+        assertThat(game.requiredRetreats.locations).containsExactlyInAnyOrder(*retreatsIn(HOL))
+
         """
         |England:
         |A Holland - Belgium
         |F North Sea - Norwegian Sea
-        |The fleet in the North Sea is not dislodge, so the move is illegal.
-        |"""
+        |""".parse(RETREATS).adjudicateAsDATC(game = game)
+
+        assertThat(game.pieces)
+            .containsEntry(BEL, England)
+            .doesNotContainKey(NWG)
     }
 
     @Test
     fun `6_H_5 TEST CASE, A UNIT MAY NOT RETREAT TO THE AREA FROM WHICH IT IS ATTACKED`() {
-        """
+        val game = """
         |Russia:
         |F Constantinople Supports F Black Sea - Ankara
         |F Black Sea - Ankara
         |
         |Turkey:
         |F Ankara Hold
-        |Fleet in Ankara is dislodged and may not retreat to Black Sea.
         |""".parse().adjudicateAsDATC()
+
+        assertThat(game.requiredRetreats.locations).containsExactlyInAnyOrder(*retreatsIn(NWY, EDI, HOL))
+
     }
 
     @Test
     fun `6_H_6 TEST CASE, UNIT MAY NOT RETREAT TO A CONTESTED AREA`() {
-        """
+        val game = """
         |Austria:
         |A Budapest Supports A Trieste - Vienna
         |A Trieste - Vienna
@@ -120,13 +132,15 @@ object TestH: WithAssertionsDATC {
         |
         |Italy:
         |A Vienna Hold
-        |The Italian army in Vienna is dislodged. It may not retreat to Bohemia.
         |""".parse().adjudicateAsDATC()
+
+        assertThat(game.requiredRetreats.locations).containsExactlyInAnyOrder(*retreatsIn(NWY, EDI, HOL))
+
     }
 
     @Test
     fun `6_H_7 TEST CASE, MULTIPLE RETREAT TO SAME AREA WILL DISBAND UNITS`() {
-        """
+        val game = """
         |Austria:
         |A Budapest Supports A Trieste - Vienna
         |A Trieste - Vienna
@@ -138,19 +152,24 @@ object TestH: WithAssertionsDATC {
         |Italy:
         |A Vienna Hold
         |A Bohemia Hold
-        |If Italy orders the following for retreat:
         |""".parse().adjudicateAsDATC()
+
+        assertThat(game.requiredRetreats.locations).containsExactlyInAnyOrder(*retreatsIn(NWY, EDI, HOL))
+
         """
         |Italy:
         |A Bohemia - Tyrolia
         |A Vienna - Tyrolia
-        |Both armies will be disbanded.
-        |"""
+        |""".parse(RETREATS).adjudicateAsDATC(game = game)
+
+        assertThat(game.pieces)
+            .doesNotContainKey(NTH)
+            .doesNotContainEntry(HOL, Russia)
     }
 
     @Test
     fun `6_H_8 TEST CASE, TRIPLE RETREAT TO SAME AREA WILL DISBAND UNITS`() {
-        """
+        val game = """
         |England:
         |A Liverpool - Edinburgh
         |F Yorkshire Supports A Liverpool - Edinburgh
@@ -165,8 +184,10 @@ object TestH: WithAssertionsDATC {
         |A Sweden Supports A Finland - Norway
         |A Finland - Norway
         |F Holland Hold
-        |The fleets in Norway, Edinburgh and Holland are dislodged. If the following retreat orders are given:
         |""".parse().adjudicateAsDATC()
+
+        assertThat(game.requiredRetreats.locations).containsExactlyInAnyOrder(*retreatsIn(NWY, EDI, HOL))
+
         """
         |England:
         |F Norway - North Sea
@@ -174,13 +195,16 @@ object TestH: WithAssertionsDATC {
         |Russia:
         |F Edinburgh - North Sea
         |F Holland - North Sea
-        |All three units are disbanded.
-        |"""
+        |""".parse(RETREATS).adjudicateAsDATC(game = game)
+
+        assertThat(game.pieces)
+            .doesNotContainKey(NTH)
+            .doesNotContainEntry(HOL, Russia)
     }
 
     @Test
     fun `6_H_9 TEST CASE, DISLODGED UNIT WILL NOT MAKE ATTACKERS AREA CONTESTED`() {
-        """
+        val game = """
         |England:
         |F Helgoland Bight - Kiel
         |F Denmark Supports F Helgoland Bight - Kiel
@@ -192,13 +216,15 @@ object TestH: WithAssertionsDATC {
         |
         |Russia:
         |A Prussia - Berlin
-        |The fleet in Kiel can retreat to Berlin.
         |""".parse().adjudicateAsDATC()
+
+        assertThat(game.requiredRetreats.locations).containsExactlyInAnyOrder(*retreatsIn(NWY, EDI, HOL))
+
     }
 
     @Test
     fun `6_H_10 TEST CASE, NOT RETREATING TO ATTACKER DOES NOT MEAN CONTESTED`() {
-        """
+        val game = """
         |England:
         |A Kiel Hold
         |
@@ -210,16 +236,21 @@ object TestH: WithAssertionsDATC {
         |Russia:
         |A Warsaw - Prussia
         |A Silesia Supports A Warsaw - Prussia
-        |The armies in Kiel and Prussia are dislodged. The English army in Kiel cannot retreat to Berlin, but the army in Prussia can retreat to Berlin. Suppose the following retreat orders are given:
         |""".parse().adjudicateAsDATC()
+
+        assertThat(game.requiredRetreats.locations).containsExactlyInAnyOrder(*retreatsIn(NWY, EDI, HOL))
+
         """
         |England:
         |A Kiel - Berlin
         |
         |Germany:
         |A Prussia - Berlin
-        |The English retreat to Berlin is illegal and fails (the unit is disbanded). The German retreat to Berlin is successful and does not bounce on the English unit.
-        |"""
+        |""".parse(RETREATS).adjudicateAsDATC(game = game)
+
+        assertThat(game.pieces)
+            .doesNotContainKey(NTH)
+            .doesNotContainEntry(HOL, Russia)
     }
 
     //6.H.11. TEST CASE, RETREAT WHEN DISLODGED BY ADJACENT CONVOY
@@ -230,7 +261,7 @@ object TestH: WithAssertionsDATC {
 
     @Test
     fun `6_H_14 TEST CASE, NO RETREAT WITH SUPPORT IN MOVEMENT PHASE`() {
-        """
+        val game = """
         |England:
         |A Picardy Hold
         |F English Channel Supports A Picardy - Belgium
@@ -243,16 +274,21 @@ object TestH: WithAssertionsDATC {
         |Germany:
         |A Munich Supports A Marseilles - Burgundy
         |A Marseilles - Burgundy
-        |After the movement phase the following retreat orders are given:
         |""".parse().adjudicateAsDATC()
+
+        assertThat(game.requiredRetreats.locations).containsExactlyInAnyOrder(*retreatsIn(NWY, EDI, HOL))
+
         """
         |England:
         |A Picardy - Belgium
         |
         |France:
         |A Burgundy - Belgium
-        |Both the army in Picardy and Burgundy are disbanded.
-        |"""
+        |""".parse(RETREATS).adjudicateAsDATC(game = game)
+
+        assertThat(game.pieces)
+            .doesNotContainKey(NTH)
+            .doesNotContainEntry(HOL, Russia)
     }
 
     //6.H.15. TEST CASE, NO COASTAL CRAWL IN RETREAT
