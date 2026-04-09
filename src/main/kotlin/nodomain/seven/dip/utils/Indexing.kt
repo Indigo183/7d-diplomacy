aeedfa25c1cc85ccd81b2b2fd31b41639b8e7185
Rua Fuller <ruafuller@gmail.com>
2026-04-09 20:53:54 +0100

    Add an interface for shifting BoardIndexes and Locations

--- src/main/kotlin/nodomain/seven/dip/utils/Indexing.kt
+++ src/main/kotlin/nodomain/seven/dip/utils/Indexing.kt
@@ -5,9 +5,12 @@ import nodomain.seven.dip.provinces.Province
 // Location of a board
 // A null timeplane represents that a board is in Limbo
 data class BoardIndex(val coordinate: ComplexNumber, var timeplane: Int? = 0) {
+    operator fun plus(other: ComplexNumber): BoardIndex = BoardIndex(coordinate + other, timeplane)
     override fun toString(): String =
         if (timeplane === null) "($coordinate, Limbo)" else "($coordinate, T$timeplane)"
 }
 
 // Location of a unit/province
-data class Location(val province: Province, val boardIndex: BoardIndex)
+data class Location(val province: Province, val boardIndex: BoardIndex) {
+    operator fun plus(other: ComplexNumber): Location = Location(province, boardIndex + other)
+}
