57aa1e4289b4dd6b2d18b1dbf49c79f518b951b6
Rua Fuller <ruafuller@gmail.com>
2026-04-17 21:09:05 +0100

    Remove dioxus server

--- frontend/src/views/home.rs
+++ frontend/src/views/home.rs
@@ -1,11 +1,10 @@
+use crate::components::Hero;
 use dioxus::prelude::*;
-use crate::components::{Hero, Echo};
 
 /// The Home page component that will be rendered when the current route is `[Route::Home]`
 #[component]
 pub fn Home() -> Element {
     rsx! {
         Hero {}
-        Echo {}
     }
 }
