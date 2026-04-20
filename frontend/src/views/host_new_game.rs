4a7047dbe7c4238714a12c27f403a21836038bf8
Rua Fuller <ruafuller@gmail.com>
2026-04-20 21:28:03 +0100

    Implement `HostNewGame`

--- frontend/src/views/host_new_game.rs
+++ frontend/src/views/host_new_game.rs
@@ -1,7 +1,73 @@
 use dioxus::prelude::*;
 
+enum Adjacencies {
+    Strict,
+    Loose,
+    NotSelected,
+}
+impl std::fmt::Display for Adjacencies {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Strict => write!(f, "Strict"),
+            Self::Loose => write!(f, "Loose"),
+            Self::NotSelected => write!(f, ""),
+        }
+    }
+}
+
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
+
 /// The menu for hosting a new game locally.
 #[component]
 pub fn HostNewGame() -> Element {
-    rsx! {}
+    let mut name = use_signal(|| String::new());
+    let mut adjacencies = use_signal(|| Adjacencies::NotSelected);
+
+    rsx! {
+        div {
+            class: "menu",
+            img { src: HEADER_SVG, id: "header" }
+            div {
+                class: "menu-options",
+                input {
+                    placeholder: "Game Name",
+                    value: "{name().to_ascii_uppercase()}", // avoids flashing lowercase
+                    oninput: move |event| name.set(event.value().to_ascii_uppercase()),
+                }
+                div {
+                    id: "adjacencies",
+                    button {
+                        id: "left",
+                        style: if let Adjacencies::Strict = *adjacencies.read() {
+                            "background-color: #203030;"
+                        },
+                        onclick: move |_event| adjacencies.set(Adjacencies::Strict),
+                        "Strict"
+                    }
+                    button {
+                        id: "right",
+                        style: if let Adjacencies::Loose = *adjacencies.read() {
+                            "background-color: #203030;"
+                        },
+                        onclick: move |_event| adjacencies.set(Adjacencies::Loose),
+                        "Loose"
+                    }
+                }
+                button {
+                    color: if let Adjacencies::NotSelected = *adjacencies.read() { "gray" }
+                    else if name().is_empty() { "gray" },
+                    onclick: match *adjacencies.read() {
+                        Adjacencies::NotSelected => |_event| println!("no"),
+                        _ if name().is_empty() => |_event| println!("no"),
+                        _ => |_event| println!("yes"),
+                    },
+                    "Submit"
+                }
+            }
+            div {
+                p { "NAME: \"{name}\"" }
+                p { "ADJACENCIES: {adjacencies}" }
+            }
+        }
+    }
 }
