faa0b6e898ee6a9ef9a2601eee238392ac838831
LudoBC <ludobvw@gmail.com>
2026-04-20 19:38:09 +0200

    crearting and loading games

--- src/main/kotlin/nodomain/seven/dip/game/GameDAO.kt
+++ src/main/kotlin/nodomain/seven/dip/game/GameDAO.kt
@@ -1,5 +1,18 @@
 package nodomain.seven.dip.game
 
+import nodomain.seven.dip.adjudication.adjudicate
+import nodomain.seven.dip.orders.A
+import nodomain.seven.dip.orders.Build
+import nodomain.seven.dip.orders.T
+import nodomain.seven.dip.orders.input
+import nodomain.seven.dip.provinces.Romans.BRU
+import nodomain.seven.dip.provinces.Romans.CAE
+import nodomain.seven.dip.provinces.Romans.CAT
+import nodomain.seven.dip.provinces.Romans.POM
+import nodomain.seven.dip.utils.Location
+import nodomain.seven.dip.utils.c
+import nodomain.seven.dip.utils.i
+import nodomain.seven.dip.utils.plus
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 import java.io.FileInputStream
@@ -9,33 +22,64 @@ import java.io.ObjectOutputStream
 import java.nio.file.Files
 import kotlin.io.path.Path
 
-
-
 object GameDAO {
     val filePath = Path(System.getProperty("user.home"), ".7dip")
-    lateinit var currentGameName: String
-    lateinit var currentGame: Game
 
     init {
         if (!Files.exists(filePath)) Files.createDirectories(filePath)
-        if (!Files.exists(filePath.resolve("currentGame.txt"))) Files.createFile(filePath.resolve("currentGame.txt"))
-        val testGamePath = filePath.resolve(Path("testGame", "gameObject.ser"))
-        if (!Files.exists(testGamePath)) {
-            Files.createDirectories(filePath.resolve("testGame"))
-            Files.createFile(testGamePath)
-            ObjectOutputStream(BufferedOutputStream(FileOutputStream(testGamePath.toFile()))).use { it.writeObject(newTestGame()) }
+        if (!Files.exists(filePath.resolve(Path("testGame", "gameObject.ser")))) {
+            storeGame("testGame", newTestGame())
         }
-        loadCurrentGameName()
-        loadCurrentGame()
     }
 
-    fun loadCurrentGameName() {
-        currentGameName = Files.newBufferedReader(filePath.resolve("currentGame.txt")).use { it.readLine() ?: "testGame" }
-    }
-    fun loadCurrentGame() {
-        val saveGamePath = filePath.resolve(currentGameName).resolve("gameObject.ser")
-        currentGame = ObjectInputStream(BufferedInputStream(FileInputStream(saveGamePath.toFile()))).use {
+    fun loadGame(name: String): Game {
+        val saveGamePath = filePath.resolve(name).resolve("gameObject.ser")
+        return ObjectInputStream(BufferedInputStream(FileInputStream(saveGamePath.toFile()))).use {
             it.readObject() as Game
         }
     }
+
+    fun existingGame(name: String) = Files.exists(filePath.resolve(name))
+
+    fun storeGame(name: String, game: Game) {
+        val gamePath = filePath.resolve(name)
+        Files.createDirectory(gamePath)
+        Files.createFile(gamePath.resolve("gameObject.ser"))
+        ObjectOutputStream(BufferedOutputStream(FileOutputStream(gamePath.resolve("gameObject.ser").toFile()))).use { it.writeObject(game) }
+    }
+}
+
+fun newTestGame(): Game {
+    val origin = T(0.c, 0)
+
+    val game = Game()
+
+    game.input(listOf(
+        origin A CAT M BRU i 2,
+        origin A POM M BRU i 1,
+    ))
+    game.adjudicate()
+
+    game.input(listOf(
+        T(i, 0) A CAT M Location(CAT, origin) i 2,
+        T(i, 0) A BRU M Location(CAT, origin) i 2,
+
+        T(-1.c, 0) A BRU M POM i 1,
+        T(-1.c, 0) A POM M Location(POM, origin) i 1,
+    ))
+    game.adjudicate()
+    game.input(listOf(Build(T(-1+i, 0) A CAT)))
+    game.adjudicate()
+
+    game.input(listOf(
+        T(-1+i, 0) A CAT M Location(CAE, T(i, 0)) i 3,
+        T(-1+i, 0) A POM M Location(POM, T(i, 0)) i 3,
+
+        T(i, 1) A CAT S { T(-1+i, 0) A CAT M Location(CAE, T(i, 0)) },
+        T(i, 1) A POM M CAE i 3,
+        T(i, 1) A BRU S { T(i, 1) A POM M CAE },
+    ))
+    game.adjudicate()
+
+    return game
 }
