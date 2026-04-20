93c32ff5ed71169a5128129c16740bbd6f1d20f1
Rua Fuller <ruafuller@gmail.com>
2026-04-20 01:36:08 +0100

    WIP: Almost done with `HostNewGame`

--- frontend/src/components/main_menu.rs
+++ frontend/src/components/main_menu.rs
@@ -9,10 +9,10 @@ pub fn MainMenu() -> Element {
         // We can create elements inside the rsx macro with the element name followed by a block of attributes and children.
         div {
             // Attributes should be defined in the element before any children
-            id: "menu",
+            class: "menu",
             // After all attributes are defined, we can define child elements and components
             img { src: HEADER_SVG, id: "header" }
-            div { id: "menu-options",
+            div { class: "menu-options",
                 // The RSX macro also supports text nodes surrounded by quotes
                 Link { to: Route::ResumeGame {}, "Resume Game" }
                 Link { to: Route::JoinNewGame {}, "Join New Game" }
--- frontend/src/views/host_new_game.rs
+++ frontend/src/views/host_new_game.rs
@@ -1,20 +1,62 @@
 use dioxus::prelude::*;
 
+enum Adjacencies {
+    Strict,
+    Loose,
+    NotSelected,
+}
+impl std::fmt::Display for Adjacencies {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Strict => write!(f, "Strict"),
+            Self::Loose => write!(f, "Loose"),
+            Self::NotSelected => write!(f, ""),
+        }
+    }
+}
+
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
 /// The menu for hosting a new game locally.
 #[component]
 pub fn HostNewGame() -> Element {
+    let mut name = use_signal(|| String::new());
+    let mut adjacencies = use_signal(|| Adjacencies::NotSelected);
+
     rsx! {
         div {
-            id: "menu",
+            class: "menu",
             img { src: HEADER_SVG, id: "header" }
-            div { id: "menu-options",
-                input { placeholder: "game name" }
-                select {
-                    option { "Strict" }
-                    option { "Loose" }
+            div {
+                class: "menu-options",
+                input {
+                    placeholder: "Game Name",
+                    value: "{name().to_ascii_uppercase()}", // avoids flashing lowercase
+                    oninput: move |event| name.set(event.value().to_ascii_uppercase()),
                 }
+                div {
+                    id: "adjacencies",
+                    button {
+                        id: "left",
+                        style: if let Adjacencies::Strict = *adjacencies.read() {
+                            "background-color: #203030;"
+                        },
+                        onclick: move |_event| adjacencies.set(Adjacencies::Strict),
+                        "Strict"
+                    }
+                    button {
+                        id: "right",
+                        style: if let Adjacencies::Loose = *adjacencies.read() {
+                            "background-color: #203030;"
+                        },
+                        onclick: move |_event| adjacencies.set(Adjacencies::Loose),
+                        "Loose"
+                    }
+                }
+            }
+            div {
+                p { "NAME: {name}" }
+                p { "ADJACENCIES: {adjacencies}" }
             }
         }
     }
