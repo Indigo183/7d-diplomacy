2896f89fd1ad0f886178e6c5ab89d105faafa6e7
Rua Fuller <ruafuller@gmail.com>
2026-07-07 21:52:32 +0100

    Move game configuration into src/settings.rs

--- frontend/src/settings.rs
+++ frontend/src/settings.rs
@@ -1,4 +1,23 @@
-#[derive(PartialEq, Clone, Copy)]
+#[derive(PartialEq, Eq, Clone, Copy)]
+pub enum TimeTravel {
+    FiveDimensional,
+    SevenDimensional,
+}
+impl std::fmt::Display for TimeTravel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::FiveDimensional => write!(f, "5D"),
+            Self::SevenDimensional => write!(f, "7D"),
+        }
+    }
+}
+impl Default for TimeTravel {
+    fn default() -> Self {
+        Self::SevenDimensional
+    }
+}
+
+#[derive(PartialEq, Eq, Clone, Copy)]
 pub enum Adjacencies {
     Strict,
     Loose,
@@ -8,6 +27,108 @@ impl std::fmt::Display for Adjacencies {
         match self {
             Self::Strict => write!(f, "Strict"),
             Self::Loose => write!(f, "Loose"),
+        }
+    }
+}
+impl Default for Adjacencies {
+    fn default() -> Self {
+        Self::Strict
+    }
+}
+
+/// The variant data for a specific variant, parsed from JSON.
+pub struct Variant {
+    /// The variant's name (not including time travel details).
+    pub name: String,
+    /// The variant's map data (TODO: NOT IMPLEMENTED).
+    map: (),
+    /// The variant's player list.
+    player_list: Vec<String>,
+}
+impl Default for Variant {
+    fn default() -> Self {
+        Self {
+            name: String::from("Romans"),
+            map: (),
+            player_list: vec![String::from("Cato"), String::from("Pompey")],
+        }
+    }
+}
+
+/// The configuration for a given game instance (e.g "5D Diplomacy AC").
+pub struct GameConfig {
+    /// The game's non-unique official name.
+    pub name: String,
+    /// The game's unique alphanumeric identifier.
+    pub id: String,
+    /// The game's variant data.
+    pub variant: Variant,
+    /// The game's time travel details.
+    pub time_travel: TimeTravel,
+    /// The game's adjacency settings.
+    pub adjacencies: Adjacencies,
+}
+impl GameConfig {
+    /// Instantiates a builder for `settings::GameConfig`.
+    pub fn builder() -> GameConfigBuilder {
+        GameConfigBuilder {
+            name: String::new(),
+            id: String::new(),
+            variant: Variant::default(),
+            time_travel: TimeTravel::default(),
+            adjacencies: Adjacencies::default(),
+        }
+    }
+}
+
+/// A builder type for `settings::GameConfig`.
+pub struct GameConfigBuilder {
+    /// The game's non-unique official name.
+    name: String,
+    /// The game's unique alphanumeric identifier.
+    id: String,
+    /// The game's variant data.
+    variant: Variant,
+    /// The game's time travel details.
+    time_travel: TimeTravel,
+    /// The game's adjacency settings.
+    adjacencies: Adjacencies,
+}
+impl GameConfigBuilder {
+    /// The game's non-unique official name.
+    pub fn with_name(self, name: String) -> Self {
+        Self { name, ..self }
+    }
+    /// The game's unique alphanumeric identifier.
+    pub fn with_id(self, id: String) -> Self {
+        Self { id, ..self }
+    }
+    /// The game's variant data.
+    pub fn with_variant(self, variant: Variant) -> Self {
+        Self { variant, ..self }
+    }
+    /// The game's time travel details.
+    pub fn with_time_travel(self, time_travel: TimeTravel) -> Self {
+        Self {
+            time_travel,
+            ..self
+        }
+    }
+    /// The game's adjacency settings.
+    pub fn with_adjacencies(self, adjacencies: Adjacencies) -> Self {
+        Self {
+            adjacencies,
+            ..self
+        }
+    }
+    /// Builds an instance of `settings::GameConfig` from the builder.
+    pub fn build(self) -> GameConfig {
+        GameConfig {
+            name: self.name,
+            id: self.id,
+            variant: self.variant,
+            time_travel: self.time_travel,
+            adjacencies: self.adjacencies,
         }
     }
 }
--- frontend/src/views/join_new_game.rs
+++ frontend/src/views/join_new_game.rs
@@ -1,78 +1,32 @@
+use crate::settings::*;
+
 use dioxus::prelude::*;
 
 const THROBBER: Asset = asset!("/assets/styling/throbber.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
-enum Adjacencies {
-    Strict,
-    Loose,
-}
-impl std::fmt::Display for Adjacencies {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if let Self::Strict = self {
-            write!(f, "Strict")
-        } else {
-            write!(f, "Loose")
-        }
-    }
-}
-
-enum TimeTravel {
-    FiveDimensional,
-    SevenDimensional,
-}
-
-struct Variant {
-    /// The variant's name (not including time travel details).
-    name: String,
-    /// The variant's time travel details.
-    time_travel: TimeTravel,
-    /// The variant's map data (TODO: NOT IMPLEMENTED).
-    map: (),
-    /// The variant's player list.
-    player_list: Vec<String>,
-}
-
-struct Config {
-    /// The game's non-unique official name.
-    name: String,
-    /// The game's unique alphanumeric identifier.
-    id: String,
-    /// The game's variant data.
-    variant: Variant,
-    /// The game's adjacency settings.
-    adjacencies: Adjacencies,
-}
-
 async fn load_game(
     game_link: String,
     mut is_loading: Signal<bool>,
     mut loaded: Signal<bool>,
-) -> Result<Config, &'static str> {
+) -> Result<GameConfig, &'static str> {
     is_loading.set(true);
 
     smol::Timer::after(std::time::Duration::from_secs(2)).await;
 
     is_loading.set(false);
     loaded.set(true);
-    Ok(Config {
-        name: String::from("5D \"Double Trouble\" Diplomacy"),
-        id: game_link,
-        variant: Variant {
-            name: String::from("Double Trouble"),
-            time_travel: TimeTravel::FiveDimensional,
-            map: (),
-            player_list: vec![String::from("I'm too lazy :P")],
-        },
-        adjacencies: Adjacencies::Strict,
-    })
+    Ok(GameConfig::builder()
+        .with_name(String::from("5D \"Double Trouble\" Diplomacy"))
+        .with_id(game_link)
+        .build())
 }
 
 /// The menu for joining a new game remotely.
 #[component]
 pub fn JoinNewGame() -> Element {
     let mut game_link = use_signal(|| String::new());
-    let mut game_config: Signal<Option<Config>> = use_signal(|| None);
+    let mut game_config: Signal<Option<GameConfig>> = use_signal(|| None);
 
     // apparently don't need to be mutable???
     let is_loading = use_signal(|| false);
