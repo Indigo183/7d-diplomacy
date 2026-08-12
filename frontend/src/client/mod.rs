876fc9764c0fbd56f1b5c67aea7c93c04abe67a9
Rua Fuller <ruafuller@gmail.com>
2026-08-12 08:30:01 +0100

    Lay out API request testing

--- frontend/src/client/mod.rs
+++ frontend/src/client/mod.rs
@@ -1,36 +1,64 @@
+#[cfg(test)]
+mod test;
+
 use reqwest;
+use reqwest::Url;
+use std::sync::LazyLock;
+
+pub const DEFAULT_URL: LazyLock<Url> =
+    LazyLock::new(|| Url::parse("http://localhost:9090/api/game/").unwrap());
 
 //-- /api/game --//
 
 // GET
-pub fn get_game_names() {}
+pub async fn get_game_names(url: Url) -> anyhow::Result<Vec<String>> {
+    todo!()
+}
 // POST
-pub fn create_game() {}
+pub async fn create_game(url: Url, id: &str) -> anyhow::Result<()> {
+    todo!()
+}
 
 //-- /api/game/{id} --//
 
 // PATCH
-pub fn gm_action() {}
+pub async fn gm_action(url: Url) -> anyhow::Result<()> {
+    todo!()
+}
 // POST
-pub fn get_player_token() {}
+pub async fn get_player_token(url: Url) -> anyhow::Result<()> {
+    todo!()
+}
 // GET
-pub fn get_game() {}
+pub async fn get_game(url: Url) -> anyhow::Result<()> {
+    todo!()
+}
 
 //-- /api/game/{id}/{country} --//
 
 // POST
-pub fn post_json_orders() {}
+pub async fn post_json_orders(url: Url) -> anyhow::Result<()> {
+    todo!()
+}
 // GET
-pub fn get_orders() {}
+pub async fn get_orders(url: Url) -> anyhow::Result<()> {
+    todo!()
+}
 
 //-- /api/game/{id}/{country}/ready --//
 
 // POST
-pub fn set_ready() {}
+pub async fn set_ready(url: Url) -> anyhow::Result<()> {
+    todo!()
+}
 // GET
-pub fn get_ready() {}
+pub async fn get_ready(url: Url) -> anyhow::Result<()> {
+    todo!()
+}
 
 //-- /api/game/{id}/{country}/token-log --//
 
 // GET
-pub fn get_token_access_log() {}
+pub async fn get_token_access_log(url: Url) -> anyhow::Result<()> {
+    todo!()
+}
---
+++ frontend/src/client/test.rs
@@ -0,0 +1,61 @@
+use super::*;
+
+#[tokio::test]
+pub async fn test_get_game_names() {
+    let result = get_game_names(DEFAULT_URL.clone()).await;
+    let _ = dbg!(result);
+}
+
+#[tokio::test]
+pub async fn test_create_game() {
+    let result = create_game(DEFAULT_URL.clone(), "test-game").await;
+    let _ = dbg!(result);
+}
+
+#[tokio::test]
+pub async fn test_gm_action() {
+    let result = gm_action(DEFAULT_URL.clone()).await;
+    let _ = dbg!(result);
+}
+
+#[tokio::test]
+pub async fn test_get_player_token() {
+    let result = get_player_token(DEFAULT_URL.clone()).await;
+    let _ = dbg!(result);
+}
+
+#[tokio::test]
+pub async fn test_get_game() {
+    let result = get_game(DEFAULT_URL.clone()).await;
+    let _ = dbg!(result);
+}
+
+#[tokio::test]
+pub async fn test_post_json_orders() {
+    let result = post_json_orders(DEFAULT_URL.clone()).await;
+    let _ = dbg!(result);
+}
+
+#[tokio::test]
+pub async fn test_get_orders() {
+    let result = get_orders(DEFAULT_URL.clone()).await;
+    let _ = dbg!(result);
+}
+
+#[tokio::test]
+pub async fn test_set_ready() {
+    let result = set_ready(DEFAULT_URL.clone()).await;
+    let _ = dbg!(result);
+}
+
+#[tokio::test]
+pub async fn test_get_ready() {
+    let result = get_ready(DEFAULT_URL.clone()).await;
+    let _ = dbg!(result);
+}
+
+#[tokio::test]
+pub async fn test_get_token_access_log() {
+    let result = get_token_access_log(DEFAULT_URL.clone()).await;
+    let _ = dbg!(result);
+}
