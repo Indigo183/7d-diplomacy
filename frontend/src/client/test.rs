f36caead098f1689260593d034837ac9228a88c9
Rua Fuller <ruafuller@gmail.com>
2026-08-18 09:08:54 +0100

    Write rudimentary tests to be manually run

--- frontend/src/client/test.rs
+++ frontend/src/client/test.rs
@@ -16,7 +16,7 @@ pub async fn test_create_game() {
 
 #[tokio::test]
 pub async fn test_gm_action() {
-    let result = gm_action(DEFAULT_URL.clone(), "test-game", "eyJhbGciOiJIUzI1NiJ9.eyJnYW1lSWQiOiJ0ZXN0LWdhbWUiLCJpc0dNIjp0cnVlfQ.ZZUGfKi3DPcpyYjlLNW1fFmJvSQlwwdeYut8rwOS9fk", SetProperty(Started)).await;
+    let result = gm_action(DEFAULT_URL.clone(), "test-game", "eyJhbGciOiJIUzI1NiJ9.eyJnYW1lSWQiOiJ0ZXN0LWdhbWUiLCJpc0dNIjp0cnVlfQ.LgLGEMzAywZZze5cRBKJlc0jbpnWUyzA1RlOLO6fSfw", SetProperty(Started)).await;
     let _ = dbg!(result);
 }
 
@@ -34,30 +34,30 @@ pub async fn test_get_game() {
 
 #[tokio::test]
 pub async fn test_post_json_orders() {
-    let result = post_json_orders(DEFAULT_URL.clone(), "eyJhbGciOiJIUzI1NiJ9.eyJnYW1lSWQiOiJ0ZXN0LWdhbWUiLCJjb3VudHJ5IjoiQ2F0byJ9.6KNmg6TetboEy0zwpRWOsgX4VUIaVqlGWLnnaGsnLHk", "test-game", "Cato", vec![]).await;
+    let result = post_json_orders(DEFAULT_URL.clone(), "eyJhbGciOiJIUzI1NiJ9.eyJnYW1lSWQiOiJ0ZXN0LWdhbWUiLCJjb3VudHJ5IjoiQ2F0byJ9.rjbCXxV4Dbix7tri4_Ld-5NTLcdLr1pyq0MzmcnCXQs", "test-game", "Cato", vec![]).await;
     let _ = dbg!(result);
 }
 
 #[tokio::test]
 pub async fn test_get_orders() {
-    let result = get_orders(DEFAULT_URL.clone(), "eyJhbGciOiJIUzI1NiJ9.eyJnYW1lSWQiOiJ0ZXN0LWdhbWUiLCJjb3VudHJ5IjoiQ2F0byJ9.6KNmg6TetboEy0zwpRWOsgX4VUIaVqlGWLnnaGsnLHk", "test-game", "Cato").await;
+    let result = get_orders(DEFAULT_URL.clone(), "eyJhbGciOiJIUzI1NiJ9.eyJnYW1lSWQiOiJ0ZXN0LWdhbWUiLCJjb3VudHJ5IjoiQ2F0byJ9.rjbCXxV4Dbix7tri4_Ld-5NTLcdLr1pyq0MzmcnCXQs", "test-game", "Cato").await;
     let _ = dbg!(result);
 }
 
 #[tokio::test]
 pub async fn test_set_ready() {
-    let result = set_ready(DEFAULT_URL.clone(), "eyJhbGciOiJIUzI1NiJ9.eyJnYW1lSWQiOiJ0ZXN0LWdhbWUiLCJjb3VudHJ5IjoiQ2F0byJ9.6KNmg6TetboEy0zwpRWOsgX4VUIaVqlGWLnnaGsnLHk", "test-game", "Cato", true).await;
+    let result = set_ready(DEFAULT_URL.clone(), "eyJhbGciOiJIUzI1NiJ9.eyJnYW1lSWQiOiJ0ZXN0LWdhbWUiLCJjb3VudHJ5IjoiQ2F0byJ9.rjbCXxV4Dbix7tri4_Ld-5NTLcdLr1pyq0MzmcnCXQs", "test-game", "Cato", true).await;
     let _ = dbg!(result);
 }
 
 #[tokio::test]
 pub async fn test_get_ready() {
-    let result = get_ready(DEFAULT_URL.clone(), "toeyJhbGciOiJIUzI1NiJ9.eyJnYW1lSWQiOiJ0ZXN0LWdhbWUiLCJjb3VudHJ5IjoiQ2F0byJ9.6KNmg6TetboEy0zwpRWOsgX4VUIaVqlGWLnnaGsnLHksken", "test-game", "Cato").await;
+    let result = get_ready(DEFAULT_URL.clone(), "eyJhbGciOiJIUzI1NiJ9.eyJnYW1lSWQiOiJ0ZXN0LWdhbWUiLCJjb3VudHJ5IjoiQ2F0byJ9.rjbCXxV4Dbix7tri4_Ld-5NTLcdLr1pyq0MzmcnCXQs", "test-game", "Cato").await;
     let _ = dbg!(result);
 }
 
 #[tokio::test]
 pub async fn test_get_token_access_log() {
-    let result = get_token_access_log(DEFAULT_URL.clone(), "toeyJhbGciOiJIUzI1NiJ9.eyJnYW1lSWQiOiJ0ZXN0LWdhbWUiLCJjb3VudHJ5IjoiQ2F0byJ9.6KNmg6TetboEy0zwpRWOsgX4VUIaVqlGWLnnaGsnLHksken", "test-game", "Cato").await;
+    let result = get_token_access_log(DEFAULT_URL.clone(), "eyJhbGciOiJIUzI1NiJ9.eyJnYW1lSWQiOiJ0ZXN0LWdhbWUiLCJjb3VudHJ5IjoiQ2F0byJ9.rjbCXxV4Dbix7tri4_Ld-5NTLcdLr1pyq0MzmcnCXQs", "test-game", "Cato").await;
     let _ = dbg!(result);
 }
